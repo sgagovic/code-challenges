f2d385a5da8e7482f445d4852d2ca2fda99d282b
Sanja Gagovic <sanja.gagovic@embriq.no>
2024-04-27 09:15:17 +0200

    fixed anagram so it returns correct values

--- src/main/java/org/priv/playground/Anagram.java
+++ src/main/java/org/priv/playground/Anagram.java
@@ -7,37 +7,39 @@ import java.util.stream.Collectors;
 public class Anagram {
 
     private List<String> places = Arrays.asList("eat", "ate", "apt", "pat", "tea", "now");
-    private Map<Integer, List<String>> map = new HashMap<>();
+    Map<String, List<String>> result = new HashMap<>();
 
 
     public Map<String, List<String>> isAnagram(){
+       List<String> sorted = sort(places);
 
+        for(int i=0; i<sorted.size(); i++){
+            List<String> anagrams = new ArrayList<>();
+            if(result.containsKey(sorted.get(i))){
+                continue;
+            } else{
+                result.put(sorted.get(i), anagrams);
+                for(int j=0; j<sorted.size(); j++){
+                    if(sorted.get(i).equals(sorted.get(j))){
+                        anagrams.add(places.get(j));
+                    }
+                }
+            }
+            result.put(sorted.get(i), anagrams);
+        }
+
+        return result;
+    }
+
+    private List<String> sort(List<String> elements){
         ArrayList<String> sorted = new ArrayList<>();
-        for(int i=0; i<places.size(); i++){
-            char[] chars = places.get(i).toCharArray();
+        for(int i=0; i<elements.size(); i++){
+            char[] chars = elements.get(i).toCharArray();
             Arrays.sort(chars);
             sorted.add(String.valueOf(chars));
         }
 
-        Map<String, List<String>> result = new HashMap<>();
-        for(int i=0; i<sorted.size(); i++){
-            List<String> anagrams = new ArrayList<>();
-
-            if(!result.containsKey(places.get(i))){
-                anagrams.add(places.get(i));
-                result.put(places.get(i), anagrams);
-            }
-
-            for(int j=1; j<sorted.size(); j++){
-                if(sorted.get(i).equals(sorted.get(j))){
-                    anagrams.add(places.get(j));
-                }
-            }
-
-            result.put(places.get(i), anagrams);
-        }
-
-        return result;
+        return sorted;
     }
 
 }
